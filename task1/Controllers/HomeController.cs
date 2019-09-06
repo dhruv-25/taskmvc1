4eea858f7b85ba460efb26070b19799ccbed4cae
Dhruv Suthar <dhruvsuthar025@hotmail.com>
2019-09-06 19:50:01 +0530

    Add project files.

---
+++ task1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult MainPage()
+        {
+            string imgPath = Server.MapPath("~/Content/Image/mobile.jpg");
+            byte[] byteData = System.IO.File.ReadAllBytes(imgPath);
+            string imgBase64Data = Convert.ToBase64String(byteData);
+            string imgDataURL = string.Format("data:image/jpg;base64,{0}", imgBase64Data);
+            ViewBag.ImageData = imgDataURL;
+            return View();
+        }
+    }
+}
